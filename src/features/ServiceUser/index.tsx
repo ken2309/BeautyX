a2aa4855953f3a4cdc92d31def4b3ee6409576e2
Đỗ Cao Long <long@myspa.vn>
2022-05-27 10:27:49 +0700

    [UPDATE] fix calendar no choose date before

--- src/features/ServiceUser/index.tsx
+++ src/features/ServiceUser/index.tsx
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
-import Head from '../Head';
-import { Container } from '@mui/material';
-import './mySer.css';
-import ServiceBook from './components/ServiceBook';
-import PopupNoti from './components/PopupNoti';
-import ServiceBottom from './components/ServiceBottom';
-import Footer from '../Footer';
-import { useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import Head from "../Head";
+import { Container } from "@mui/material";
+import "./mySer.css";
+import ServiceBook from "./components/ServiceBook";
+import PopupNoti from "./components/PopupNoti";
+import ServiceBottom from "./components/ServiceBottom";
+import Footer from "../Footer";
+import { useLocation } from "react-router-dom";
 
 function ServicesUser(props: any) {
     const { setOpen } = props;
     const location = useLocation();
     const prev_url = location.state;
-    const [openNoti, setOpenNoti] = useState(true)
+    const [openNoti, setOpenNoti] = useState(true);
     //mb
     return (
         <>
-            <Head
-                setCloseDialog={setOpen}
-                prev_url={prev_url}
-            />
+            <Head setCloseDialog={setOpen} prev_url={prev_url} />
             <Container>
                 <div className="flex-row-sp my-ser">
                     <div className="my-ser__right">
@@ -30,10 +27,9 @@ function ServicesUser(props: any) {
                     </div>
                 </div>
             </Container>
-            <ServiceBottom
-            //servicesBook={servicesBook}
-            //setServicesBook={setServicesBook}
-            />
+            {/* <ServiceBottom
+           
+            /> */}
             <ServiceBottom
                 //servicesBook={servicesBook}
                 setOpenNoti={setOpenNoti}
