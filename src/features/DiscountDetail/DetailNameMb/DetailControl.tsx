5c906c5122df6e472bffdc43956ad9e788878cd5
Đỗ Cao Long <long@myspa.vn>
2022-06-01 11:01:43 +0700

    [UPDATE] add tab in page discount promo

---
+++ src/features/DiscountDetail/DetailNameMb/DetailControl.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Drawer } from "@mui/material";
+import DetailCard from "../../ProductDetail/components/DetailCard";
+
+function DetailControl(props: any) {
+    const { open, setOpen, detail, is_type, org } = props;
+    return (
+        <Drawer open={open} anchor="bottom" onClose={() => setOpen(false)}>
+            <div className="mb-de-control">
+                <div className="mb-de-control_x"></div>
+                <DetailCard product={detail} is_type={is_type} org={org} />
+            </div>
+        </Drawer>
+    );
+}
+
+export default DetailControl;
--- src/features/DiscountDetail/DetailNameMb/index.tsx
+++ src/features/DiscountDetail/DetailNameMb/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import formatPrice from "../../utils/formatPrice";
+import formatPrice from "../../../utils/formatPrice";
 import DetailControl from "./DetailControl";
 import "./detailNameMb.css";
 
@@ -7,31 +7,40 @@ function DetailNameMb(props: any) {
     const { detail, is_type, org } = props;
     const [old_price, setOld_price] = useState(0);
     const [sale_price, setSale_price] = useState(0);
+    const detailDiscountItem = detail.productable;
+    // console.log("detailDiscountItem", detailDiscountItem);
     const [open, setOpen] = useState(false);
     useEffect(() => {
         if (is_type === 1 || is_type === "SERVICE") {
-            if (detail?.special_price > 0) {
-                setSale_price(detail?.special_price);
-                setOld_price(detail?.retail_price);
+            if (detail?.view_price > 0) {
+                setSale_price(detail?.view_price);
+                setOld_price(detailDiscountItem?.retail_price);
             } else {
                 setSale_price(detail?.retail_price);
             }
         } else if (is_type === 2 || is_type === "PRODUCT") {
-            if (detail?.special_price > 0) {
-                setSale_price(detail?.special_price);
-                setOld_price(detail?.price);
+            if (detail?.view_price > 0) {
+                setSale_price(detail?.view_price);
+                setOld_price(detailDiscountItem?.price);
             } else {
                 setSale_price(detail?.price);
             }
         }
-    }, [is_type, detail?.price, detail?.retail_price, detail?.special_price]);
+    }, [
+        is_type,
+        detailDiscountItem?.price,
+        detail?.retail_price,
+        detail?.view_price,
+        detail?.price,
+        detailDiscountItem?.retail_price,
+    ]);
     return (
         <>
             <div className="detail-name-mb">
                 <div className="detail-name-mb__name">
-                    {detail?.product_name
-                        ? detail?.product_name
-                        : detail?.service_name}
+                    {detailDiscountItem?.service_name
+                        ? detailDiscountItem?.service_name
+                        : detailDiscountItem?.product_name}
                 </div>
                 <div className="detail-name-mb__price">
                     <span
