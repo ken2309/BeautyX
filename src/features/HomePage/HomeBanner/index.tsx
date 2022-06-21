41e568958a6dae5e0798f21741bf76279f87459e
Đỗ Cao Long <long@myspa.vn>
2022-06-21 17:34:40 +0700

    [UPDATE] banner home

--- src/features/HomePage/HomeBanner/index.tsx
+++ src/features/HomePage/HomeBanner/index.tsx
@@ -1,10 +1,15 @@
 import { Container } from "@mui/system";
 import React, { useState } from "react";
+import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import Slider from "react-slick";
 import icon from "../../../constants/icon";
 import { dealHot } from "../../../constants/img";
 import { IBanner } from "../../../interface/banner";
+import slugify from "../../../utils/formatUrlString";
+import scrollTop from "../../../utils/scrollTop";
+import HomeBannerPopup from "../../Home/components/HomeBannerPopup";
 import "./homeBanner.css";
 
 const PrevButton = (props: any) => {
@@ -23,11 +28,71 @@ const NextButton = (props: any) => {
         </button>
     );
 };
+export const deals = [
+    {
+        id: 1,
+        title: "Deal hot từ 50-100k",
+        min_price: 50000,
+        max_price: 100000,
+        img: dealHot.dealhot,
+    },
+    {
+        id: 2,
+        title: "Deal chăm sóc da làm đẹp Giảm 50%",
+        min_price: null,
+        img: dealHot.dealhot1,
+        percent: 50,
+    },
+    {
+        id: 3,
+        title: "Dịch vụ xâm lấn Giảm 30%",
+        min_price: null,
+        img: dealHot.dealhot2,
+        percent: 30,
+    },
+];
 
 export default function HomeBanner() {
-    const [chooseBanner, setChooseBanner] = useState<IBanner>();
+    const history = useHistory();
     const HOME = useSelector((state: any) => state.HOME);
     const { banners } = HOME;
+    const [chooseBanner, setChooseBanner] = useState<IBanner>();
+    const [open, setOpen] = useState(false);
+    const [openVideo, setOpenVideo] = useState(false);
+    function openWeb() {
+        const payUrl = chooseBanner?.url;
+        window.open(`${payUrl}`, "_blank", "noopener,noreferrer");
+    }
+    function closePopupVideo() {
+        setOpenVideo(false);
+    }
+    const handleClick = () => {
+        if (chooseBanner) {
+            switch (chooseBanner.type) {
+                case "VIDEO":
+                    return setOpenVideo(true);
+                case "HTML":
+                    return setOpen(true);
+                case "WEB":
+                    return openWeb();
+                case "PROMOTION":
+                    return console.log("PROMOTION");
+                case "ORGANIZATION":
+                    return history.push({
+                        pathname: `/org/${chooseBanner.origin_id}`,
+                    });
+                default:
+                    break;
+            }
+        }
+    };
+    const gotoDetail = (item: any) => {
+        scrollTop();
+        history.push({
+            pathname: `/deal/${slugify(item.title)}`,
+            search: `${item.id}`,
+        });
+    };
     const settings = {
         dots: true,
         infinite: true,
@@ -74,7 +139,7 @@ export default function HomeBanner() {
                         <Slider {...settings}>
                             {banners.map((item: any, index: number) => (
                                 <div
-                                    // onClick={handleClick}
+                                    onClick={handleClick}
                                     key={index + item.url}
                                     className="banner-slide__img"
                                 >
@@ -85,19 +150,56 @@ export default function HomeBanner() {
                     </div>
                     <div className="banner-right">
                         <div className="banner-right__top">
-                            <img src={dealHot.dealhot} alt="" />
+                            <img
+                                onClick={() => gotoDetail(deals[0])}
+                                src={dealHot.dealhot}
+                                alt=""
+                            />
                         </div>
                         <div className="banner-right__bottom">
                             <div className="banner-bottom__item">
-                                <img src={dealHot.dealhot1} alt="" />
+                                <img
+                                    onClick={() => gotoDetail(deals[1])}
+                                    src={dealHot.dealhot1}
+                                    alt=""
+                                />
                             </div>
                             <div className="banner-bottom__item">
-                                <img src={dealHot.dealhot2} alt="" />
+                                <img
+                                    onClick={() => gotoDetail(deals[2])}
+                                    src={dealHot.dealhot2}
+                                    alt=""
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
             </Container>
+            <HomeBannerPopup
+                data={chooseBanner}
+                open={open}
+                setOpen={setOpen}
+            />
+
+            {openVideo === true ? (
+                <div className="homebanner__popup-videobox">
+                    <div className="homebanner__popup-video">
+                        <span className="close-icon" onClick={closePopupVideo}>
+                            x
+                        </span>
+                        <div className="banner-video">
+                            <ReactPlayer
+                                controls
+                                width={"100%"}
+                                height={"100%"}
+                                url={`${chooseBanner?.url}`}
+                            />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
     );
 }
