7a2079726e2b273860d87e27d53ff24646f459dd
Nguyễn Quang Khải <khai@myspa.vn>
2022-06-23 17:17:49 +0700

    [UPDATE] support chat btn

---
+++ src/features/ChatBox/index.tsx
@@ -0,0 +1,11 @@
+import {handleSubiz} from '../../utils/customChat';
+import './style.css';
+import icon from '../../constants/icon';
+
+export default function chatBox(){
+    return(
+        <div className="support_chat_icon"onClick={handleSubiz}>
+            <img className="chat_ico" src={icon.supportChat} alt=""/>
+        </div>
+    )
+}
