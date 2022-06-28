4422bcf804cae098513d5076341461c06f1edef1
Đỗ Cao Long <long@myspa.vn>
2022-06-28 16:34:14 +0700

    [UPDATE] fix bug btn assis

--- src/components/AssistantBtn/index.tsx
+++ src/components/AssistantBtn/index.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import icon from "../../constants/icon";
-import img from "../../constants/img";
-import Search from "../../features/Search";
 import { onToggleSearchCnt } from "../../redux/search/searchSlice";
 import { handleSubiz } from "../../utils/customChat";
 import useFullScreen from "../../utils/useFullScreen";
+import Search from "../../features/Search";
 import "./style.css";
+import img from "../../constants/img";
 
 export default function AssistantBtn() {
     const dispatch = useDispatch();
     const { open } = useSelector((state: any) => state.SEARCH);
+    const [overLay, setOverLay] = useState(false);
+    console.log("overlay :>> ", overLay);
     const is_mb = useFullScreen();
     const history = useHistory();
-    const refOverLay: any = useRef();
-    const refAssisBtn: any = useRef();
-    const [overLay, setOverLay] = useState(false);
-    console.log("overLay :>> ", overLay);
+
     const handleOpenSearch = () => {
         const action = open ? false : true;
         dispatch(onToggleSearchCnt(action));
@@ -29,15 +28,17 @@ export default function AssistantBtn() {
         handleSubiz();
     };
 
+    const refOverLay: any = useRef();
+    const refAssisBtn: any = useRef();
     const handleClickOverlay = () => {
-        setOverLay(!overLay);
         if (is_mb === true) {
+            setOverLay(!overLay);
             if (overLay === true) {
-                refOverLay.current.classList.remove("active");
-                refAssisBtn.current.classList.remove("assistantBtn-wrap-hover");
-            } else {
                 refOverLay.current.classList.add("active");
                 refAssisBtn.current.classList.add("assistantBtn-wrap-hover");
+            } else {
+                refOverLay.current.classList.remove("active");
+                refAssisBtn.current.classList.remove("assistantBtn-wrap-hover");
             }
         }
     };
@@ -54,54 +55,61 @@ export default function AssistantBtn() {
     };
 
     return (
-        <div
-            ref={refOverLay}
-            onClick={() => handleClickOverlay()}
-            className="assistantBtn"
-        >
+        <>
             <div
-                ref={refAssisBtn}
-                onMouseEnter={() => handleHover()}
-                onMouseLeave={() => handleHoverLeave()}
-                className="assistantBtn-wrap"
+                ref={refOverLay}
+                onClick={() => handleClickOverlay()}
+                className="assistantBtn"
             >
                 <div
-                    onClick={() => handleOpenSearch()}
-                    className="btn1 buttons"
+                    ref={refAssisBtn}
+                    onMouseEnter={() => handleHover()}
+                    onMouseLeave={() => handleHoverLeave()}
+                    className="assistantBtn-wrap"
                 >
-                    <div className="btn-img">
-                        <img src={icon.search} alt="" />
+                    <div
+                        onClick={() => handleOpenSearch()}
+                        className="btn1 buttons"
+                    >
+                        <div className="btn-img">
+                            <img src={icon.search} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <div onClick={() => handleOpenSubiz()} className="btn2 buttons">
-                    <div className="btn-img">
-                        <img
-                            style={{ width: "16px" }}
-                            src={icon.chatWhite}
-                            alt=""
-                        />
+                    <div
+                        onClick={() => handleOpenSubiz()}
+                        className="btn2 buttons"
+                    >
+                        <div className="btn-img">
+                            <img
+                                style={{ width: "16px" }}
+                                src={icon.chatWhite}
+                                alt=""
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div onClick={handleGoToHome} className="btn3 buttons">
-                    <div className="btn-img">
-                        <img
-                            style={{ width: "16px" }}
-                            src={icon.homeWhite}
-                            alt=""
-                        />
+                    <div onClick={handleGoToHome} className="btn3 buttons">
+                        <div className="btn-img">
+                            <img
+                                style={{ width: "16px" }}
+                                src={icon.homeWhite}
+                                alt=""
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div id="floating-button">
-                    <div className="plus">
-                        <img src={img.beautyx} alt="" />
+                    <div id="floating-button">
+                        <div className="plus">
+                            {" "}
+                            <img src={img.beautyx} alt="" />{" "}
+                        </div>
+
+                        <img alt="" className="edit" src={icon.xWhite}></img>
                     </div>
-                    <img alt="" className="edit" src={icon.xWhite}></img>
                 </div>
             </div>
             <Search />
-        </div>
+        </>
     );
 }
