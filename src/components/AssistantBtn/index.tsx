2496e6db324da162f4b0305dae52a47c57a9b7c5
Đỗ Cao Long <long@myspa.vn>
2022-06-28 14:46:39 +0700

    [UPDATE] fix btn assis

--- src/components/AssistantBtn/index.tsx
+++ src/components/AssistantBtn/index.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import icon from "../../constants/icon";
 import { onToggleSearchCnt } from "../../redux/search/searchSlice";
 import { handleSubiz } from "../../utils/customChat";
+import useFullScreen from "../../utils/useFullScreen";
 import "./style.css";
 
 export default function AssistantBtn() {
     const dispatch = useDispatch();
     const { open } = useSelector((state: any) => state.SEARCH);
+    const [overLay, setOverLay] = useState(false);
+    const is_mb = useFullScreen();
     const history = useHistory();
+
     const handleOpenSearch = () => {
         const action = open ? false : true;
         dispatch(onToggleSearchCnt(action));
@@ -17,9 +21,48 @@ export default function AssistantBtn() {
     const handleGoToHome = () => {
         history.push("/homepage");
     };
+    const handleOpenSubiz = () => {
+        handleSubiz();
+    };
+
+    const refOverLay: any = useRef();
+    const refAssisBtn: any = useRef();
+    const handleClickOverlay = () => {
+        if (is_mb === true) {
+            setOverLay(!overLay);
+            if (overLay === true) {
+                refOverLay.current.classList.add("active");
+                refAssisBtn.current.classList.add("assistantBtn-wrap-hover");
+            } else {
+                refOverLay.current.classList.remove("active");
+                refAssisBtn.current.classList.remove("assistantBtn-wrap-hover");
+            }
+        }
+    };
+
+    const handleHover = () => {
+        if (is_mb === false) {
+            refAssisBtn.current.classList.add("assistantBtn-wrap-hover");
+        }
+    };
+    const handleHoverLeave = () => {
+        if (is_mb === false) {
+            refAssisBtn.current.classList.remove("assistantBtn-wrap-hover");
+        }
+    };
+
     return (
-        <div className="assistantBtn">
-            <div id="assistantBtn-wrap">
+        <div
+            ref={refOverLay}
+            onClick={() => handleClickOverlay()}
+            className="assistantBtn"
+        >
+            <div
+                ref={refAssisBtn}
+                onMouseEnter={() => handleHover()}
+                onMouseLeave={() => handleHoverLeave()}
+                className="assistantBtn-wrap"
+            >
                 <div
                     onClick={() => handleOpenSearch()}
                     className="btn1 buttons"
@@ -29,7 +72,7 @@ export default function AssistantBtn() {
                     </div>
                 </div>
 
-                <div onClick={() => handleSubiz()} className="btn2 buttons">
+                <div onClick={() => handleOpenSubiz()} className="btn2 buttons">
                     <div className="btn-img">
                         <img
                             style={{ width: "16px" }}
